f8b31ab238976d193ea1c3c4c9583246e579e1a8
Diego Brito <diego.nobri@gmail.com>
2022-07-31 21:11:58 -0300

    Refatoração de namespace de DTOs

--- FilmesAPI/Data/Dtos/Cinema/ReadCinemaDto.cs
+++ FilmesAPI/Data/Dtos/Cinema/ReadCinemaDto.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FilmesAPI.Data.Dtos.Cinema
+namespace FilmesAPI.Data.Dtos
 
 {
     public class ReadCinemaDto
